2fdd02525745f709fa6b140bb250c5890f11a506
nikhil08 <nmarbate99@gmail.com>
2023-09-25 11:34:31 +0530

    admin pannel changes

---
+++ resources/views/admin/layout/sidebar.blade.php
@@ -0,0 +1,38 @@
+<div class="sidebar-wrapper" data-simplebar="true">
+    <div class="sidebar-header">
+        <div>
+            <img src="{{ asset('backend/assets/images/logo-icon.png') }}" class="logo-icon" alt="logo icon">
+        </div>
+        <div>
+            <h4 class="logo-text">Rukada</h4>
+        </div>
+        <div class="toggle-icon ms-auto"><i class='bx bx-arrow-to-left'></i>
+        </div>
+    </div>
+    <!--navigation-->
+    <ul class="metismenu" id="menu">
+        <li>
+            <a href="javascript:;">
+                <div class="parent-icon"><i class='bx bx-home-circle'></i>
+                </div>
+                <div class="menu-title">Dashboard</div>
+            </a>
+
+        </li>
+        <li>
+            <a href="javascript:;" class="has-arrow">
+                <div class="parent-icon"><i class="bx bx-category"></i>
+                </div>
+                <div class="menu-title">Services</div>
+            </a>
+            <ul>
+                <li> <a href="#"><i class="bx bx-right-arrow-alt"></i>Add Service</a>
+                </li>
+                <li> <a href="#"><i class="bx bx-right-arrow-alt"></i>Manage Service</a>
+                </li>
+            </ul>
+        </li>
+       
+    </ul>
+    <!--end navigation-->
+</div>
--- routes/web.php
+++ routes/web.php
@@ -15,11 +15,11 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('admin.index');
 });
 
 Route::get('/dashboard', function () {
-    return view('dashboard');
+    return view('admin.index');
 })->middleware(['auth', 'verified'])->name('dashboard');
 
 Route::middleware('auth')->group(function () {
