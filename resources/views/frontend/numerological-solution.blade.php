257f08d79330c16a26097ce33318b4ab63f90852
harshalpusdekar2 <harshalpusdekar02@gmail.com>
2023-09-30 15:49:00 +0530

    Astromg_services

--- resources/views/frontend/numerological-solution.blade.php
+++ resources/views/frontend/numerological-solution.blade.php
@@ -67,6 +67,7 @@
                     </div>
                     <div class="card-body pt-3">
                         <h5 class="card-title" style="color:#ff4a17;">Name Analysis</h5>
+                        <hr style="color:#ff4a17;">
                         <p class="card-text">Numerologists often analyze the letters in a person's name
                             and assign numerical values to each letter based on a specific numerology system
                             (such as Pythagorean or Chaldean). These numerical values are then added up to
@@ -81,6 +82,7 @@
                     </div>
                     <div class="card-body pt-3">
                         <h5 class="card-title" style="color:#ff4a17;">Birthdate Analysis</h5>
+                        <hr style="color:#ff4a17;">
                         <p class="card-text">Numerologists also analyze a person's birthdate to
                             calculate what
                             is often called a "life path" number. This number is believed to reveal key
@@ -100,6 +102,7 @@
                         <h5 class="card-title" style="color:#ff4a17;">Personality Traits and
                             Characteristics
                         </h5>
+                        <hr style="color:#ff4a17;">
                         <p class="card-text">Numerology consultants might provide insights into a
                             person's
                             personality traits, strengths, weaknesses, and overall tendencies based on
@@ -112,10 +115,11 @@
 
                 <div class="card p-3 m-4 col-md-5 col-sm-12">
                     <div class="card-item p-3">
-                        <img src="{{asset('frontend/AstromarG/zodiac_traits.jpg')}}" class="img-fluid" alt="" style="height:15rem; width:100%;">
+                        <img src="{{asset('frontend/AstromarG/compatibility_analysis.png')}}" class="img-fluid" alt="" style="height:15rem; width:100%;">
                     </div>
                     <div class="card-body pt-3">
                         <h5 class="card-title" style="color:#ff4a17;">Compatibility Analysis</h5>
+                        <hr style="color:#ff4a17;">
                         <p class="card-text">Similar to astrology, numerology can be used to assess
                             compatibility between individuals. Numerologists might compare the life path
                             or
@@ -135,6 +139,7 @@
                     </div>
                     <div class="card-body pt-3">
                         <h5 class="card-title" style="color:#ff4a17;">Timing and Events</h5>
+                        <hr style="color:#ff4a17;">
                         <p class="card-text">Numerology can offer insights into favorable times for
                             certain life
                             events or decisions. This might include guidance on when to make career
@@ -146,10 +151,11 @@
                 <!--==================================== >Name Changes ================================-->
                 <div class="card p-3 m-4 col-md-5 col-sm-12">
                     <div class="card-item p-3">
-                        <img src="{{asset('frontend/AstromarG/event_timing.jpg')}}" class="img-fluid" alt="" style="height:15rem; width:100%;">
+                        <img src="{{asset('frontend/AstromarG/name_change.jpeg')}}" class="img-fluid" alt="" style="height:15rem; width:100%;">
                     </div>
                     <div class="card-body pt-3">
                         <h5 class="card-title" style="color:#ff4a17;">Name Changes</h5>
+                        <hr style="color:#ff4a17;">
                         <p class="card-text">: In some cases, numerologists might suggest changes to a person's
                             name (such as altering the spelling) to align it more harmoniously with their
                             numerological profile. The belief is that such changes could bring positive energy
@@ -157,10 +163,30 @@
                     </div>
                 </div>
                 <!--==================================== Business and Branding. ================================-->
-                <div class=" col-md-10 col-sm-12 mt-4 px-5"></div>
+                <div class="card p-3 m-4 col-md-5 col-sm-12">
+                    <div class="card-item p-3">
+                        <img src="{{asset('frontend/AstromarG/buisness_branding.jpeg')}}" class="img-fluid" alt="" style="height:15rem; width:100%;">
+                    </div>
+                    <div class="card-body pt-3">
+                        <h5 class="card-title" style="color:#ff4a17;">Business and Branding</h5>
+                        <hr style="color:#ff4a17;">
+                        <p class="card-text">Numerology can also be applied to business names and branding.
+                            Entrepreneurs might seek numerological insights to choose a business name that
+                            resonates with their goals and values.
+                            It's important to approach numerology with an open mind and a willingness to explore
+                            the potential insights it might provide. Like astrology and Vastu, numerology is
+                            often considered a belief system and might not be supported by scientific evidence.
+                            If you're interested in AstromarG's numerology solutions, you can research their
+                            approach, read reviews or testimonials, and make an informed decision based on your
+                            own comfort level and curiosity.
+                        </p>
+                    </div>
+                </div> 
+                
+                {{-- <div class=" col-md-10 col-sm-12 mt-4 px-5">
                 <div class="cad d-flex p-5">
                     <div class="card-item " style="width:120rem;">
-                        <img src="{{asset('frontend/AstromarG/event_timing.jpg')}}" alt="" style="height:15rem; width:100%;">
+                        <img src="{{asset('frontend/AstromarG/buisness_branding.jpeg')}}" alt="" style="height:15rem; width:100%;">
                     </div>
                     <div class="card-body ms-4">
                         <h5 class="card-title" style="color:#ff4a17;">Business and Branding</h5>
@@ -175,10 +201,10 @@
                             own comfort level and curiosity.
                         </p>
                     </div>
-                </div>
+                </div> --}}
             </div>
         </div>
-
+    </div>
 
 
 
--- resources/views/frontend/vastu-consulation.blade.php
+++ resources/views/frontend/vastu-consulation.blade.php
@@ -48,16 +48,26 @@
    }
 
    .pictures .cad img {
-      max-width: 14rem;
+      width: 100%;
+      height: 100%;
+      object-fit: cover;
    }
 
-   .pictures .card {
-      max-width: 100%;
+   .pictures .cad {
+      width: 300px;
+      border: 1px solid #FF4A17;
+      border-radius: 10px;
+      overflow: hidden;
+      margin: 10px;
+}
 
-      /* max-height: 18rem; */
-   }
+.for-img{
+   width: 100%;
+   height: 200px; 
+   overflow: hidden;
+}
 
-   @media screen(min-width:210px) {}
+   /* @media screen(min-width:210px) {} */
 </style>
 
 <div class="vastu_1">
@@ -81,168 +91,121 @@
       <h2 class="title ps-3 mb-4">Key features of Vastu Shastra include:</h2>
       <!------------------------------------------ Alignment and Directions --------------------------------------------------->
       <div class="pictures">
-         <div class="row px-4">
+         <div class="row">
 
-            <div class="cad col-sm-12 col-md-6 mb-3 d-flex bg-transparent">
-
-               <img src="{{asset('frontend/AstromarG/alignment and direction.jpg')}}" class=" " alt="...">
-
-
-               <div class="card-body">
+            <div class="cad col-sm-12 col-md-6  bg-transparent">
+               {{--  --}}
+                  <div class="for-img ">
+                  <img src="{{asset('frontend/AstromarG/alignment and direction.jpg')}}" class=" " alt="...">
+               </div>
+               {{--  --}}
+               <div class="card-body pt-4">
                   <h5 class="card-title fw-bold fs-3">1] Alignment and Directions: </h5>
                   <p class="card-text text-light">Vastu emphasizes the proper alignment of a building with
                      the cardinal directions (North, South, East, and West) to harness cosmic energies and
                      natural forces.</p>
-               </div>
+               {{-- </div>
+            </div> --}}
             </div>
-
-            <!-- <div class="col-12 col-sm-12 col-md-4 col-lg-4">
-                  <div class="cad text-center bg-transparent" style="max-width:100%; min-height:100%;">
-                  
-                     <img src="AstromarG/alignment and direction.jpg" class="for-img" style="width:44%;" alt="...">
-                  <div class="card-body">
-                        <h5 class="card-title fw-bold fs-4 p-3">1] Alignment and Directions: </h5>
-                        <p class="card-text text-light">Vastu emphasizes the proper alignment of    a building with
-                           the cardinal directions (North, South, East, and West) to harness cosmic energies and
-                           natural forces.</p>
-                     </div>
-                  </div>
-               </div>
-               <div class="col-12 col-sm-12 col-md-4 col-lg-4">
-                  <div class="cad bg-transparent" style="max-width:100%; min-height:100%;">
-                  
-                     <img src="AstromarG/alignment and direction.jpg" class="for-img " style="width:30%; height:30%;" alt="...">
-                  <div class="card-body">
-                        <h5 class="card-title fw-bold fs-3 p-3">1] Alignment and Directions: </h5>
-                        <p class="card-text text-light px-3">Vastu emphasizes the proper alignment of    a building with
-                           the cardinal directions (North, South, East, and West) to harness cosmic energies and
-                           natural forces.</p>
-                     </div>
-                  </div>
-               </div>
-               <div class="col-12 col-sm-12 col-md-4 col-lg-4">
-                  <div class="cad bg-transparent" style="max-width:100%; min-height:100%;">
-                  
-                     <img src="AstromarG/alignment and direction.jpg" class="for-img" style="width:30%; height:30%;" alt="...">
-                  <div class="card-body">
-                        <h5 class="card-title fw-bold fs-3 p-3">1] Alignment and Directions: </h5>
-                        <p class="card-text text-light px-3">Vastu emphasizes the proper alignment of    a building with
-                           the cardinal directions (North, South, East, and West) to harness cosmic energies and
-                           natural forces.</p>
-                     </div>
-                  </div>
-               </div> -->
-
-            <!-- <div class="col-12 col-sm-12 col-md-6 col-lg-6">
-                  <div class=" d-flex mb-3 bg-transparent">
-                     <img src="AstromarG/alignment and direction.jpg" class="for-img" alt="...">
-                     <div class="cad-body">
-                        <h5 class="card-title fw-bold fs-3 ">1] Alignment and Directions: </h5>
-                        <p class="card-text text-light">Vastu emphasizes the proper alignment of a building with
-                           the cardinal directions (North, South, East, and West) to harness cosmic energies and
-                           natural forces.</p>
-                     </div>
-                  </div>
-               </div> -->
-            <!-- </div> -->
+            </div>
 
             <!---------------------------------------- Five Elements ---------------------------------------------->
 
-            <div class="cad d-flex col-sm-12 col-md-6 mb-3 bg-transparent">
-               <img src="{{asset('frontend/AstromarG/Five-Element.jpg')}}" class="img-fluid " alt="...">
-               <div class="card-body">
+            <div class="cad  col-sm-12 col-md-6 bg-transparent">
+               
+                  <div class="for-img">
+                     <img src="{{asset('frontend/AstromarG/Five-Element.jpg')}}" class=" " alt="...">
+                  </div>
+                  
+                  <div class="card-body pt-4">
                   <h5 class="card-title fw-bold fs-3">2] Five Elements: </h5>
                   <p class="card-text text-light">Vastu takes into account the five elements—earth, water,
                      fire, air, and space—and aims to balance their energies within a space to create
                      harmony and positivity.</p>
                </div>
             </div>
-
+            
             <!----------------------------------- Zoning and Functions ------------------------------------------>
-            <div class="cad d-flex col-sm-12 col-md-6 mb-3 bg-transparent">
-               <img src="{{asset('frontend/AstromarG/zoning-funtctions.jpg')}}" class="img-fluid rounded-start" alt="...">
-               <div class="card-body">
-                  <h5 class="card-title fw-bold fs-3">3] Zoning and Functions: </h5>
-                  <p class="card-text text-light">Different areas within a building are associated with
-                     specific functions (e.g., sleeping, cooking, working) and elements. Vastu guides the
-                     placement and design of these zones to optimize energy flow.</p>
-               </div>
-            </div>
-
-            <!---------------------------- Entrances and Doors---------------------------------------->
-            <div class="cad col-sm-12 col-md-6 mb-3 bg-transparent">
-               <div class="row g-0">
-                  <div class="col-md-5">
-                     <img src="{{asset('frontend/AstromarG/entrace.png')}}" class="img-fluid " alt="...">
+            <div class="cad col-sm-12 col-md-6 bg-transparent">
+               
+                  <div class="for-img">
+                     <img src="{{asset('frontend/AstromarG/zoning-funtctions.jpg')}}" class=" " alt="...">
                   </div>
-                  <div class="col-md-7">
-                     <div class="card-body">
+                  
+                     <div class="card-body pt-4">
+                        <h5 class="card-title fw-bold fs-3">3] Zoning and Functions: </h5>
+                        <p class="card-text text-light">Different areas within a building are associated with
+                           specific functions (e.g., sleeping, cooking, working) and elements. Vastu guides the
+                           placement and design of these zones to optimize energy flow.</p>
+                     </div>
+                  </div>
+            
+            <!---------------------------- Entrances and Doors---------------------------------------->
+            <div class="cad col-sm-12 col-md-6 bg-transparent">
+               
+                  <div class="for-img">
+                     <img src="{{asset('frontend/AstromarG/entrace.png')}}" class=" " alt="...">
+                  </div>
+                  
+                     <div class="card-body pt-4">
                         <h5 class="card-title fw-bold fs-3">4] Entrances and Doors: </h5>
                         <p class="card-text text-light">The location and orientation of entrances and doors are
                            important to facilitate the smooth flow of positive energy and discourage negativity.
                         </p>
                      </div>
                   </div>
-               </div>
-            </div>
-
+              
             <!----------------------------------- Materials and Colors ---------------------------------------------->
 
-            <div class="card mb-3 col-sm-12 col-md-6 bg-transparent">
-               <div class="row g-0">
-                  <div class="col-md-5">
-                     <img src="{{asset('frontend/AstromarG/materials-colors.jpg')}}" class="img-fluid " alt="...">
+            <div class="cad col-sm-12 col-md-6 bg-transparent">
+               
+                  <div class="for-img">
+                     <img src="{{asset('frontend/AstromarG/materials-colors.jpg')}}" class=" " alt="...">
                   </div>
-                  <div class="col-md-7">
-                     <div class="card-body">
+                  
+                     <div class="card-body pt-4">
                         <h5 class="card-title fw-bold fs-3">5] Materials and Colors: </h5>
                         <p class="card-text text-light">Vastu recommends using specific building materials and
                            colors that are aligned with the energies of the space.</p>
                      </div>
                   </div>
-               </div>
-            </div>
-
+              
             <!----------------------------------Sacred Geometry --------------------------------------------->
-            <div class="card col-sm-12 col-md-6 mb-3 bg-transparent">
-               <div class="row g-0">
-                  <div class="col-md-5">
-                     <img src="{{asset('frontend/AstromarG/scared and geometry.jpg')}}" class="img-fluid " alt="...">
+            <div class="cad col-sm-12 col-md-6 bg-transparent">
+               
+                  <div class="for-img">
+                     <img src="{{asset('frontend/AstromarG/scared and geometry.jpg')}}" class=" " alt="...">
                   </div>
-                  <div class="col-md-7">
-                     <div class="card-body">
+                  
+                     <div class="card-body pt-4">
                         <h5 class="card-title fw-bold fs-3">6] Sacred Geometry: </h5>
                         <p class="card-text text-light">Vastu Shastra incorporates sacred geometric principles and
                            proportions to create spaces that resonate with universal harmony.</p>
                      </div>
                   </div>
-               </div>
-            </div>
-
-            <!----------------------------------Balance of Energies----------------------------------->
-            <div class="card col-sm-12 col-md-6 mb-3 bg-transparent">
-               <div class="row g-0">
-                  <div class="col-md-5">
-                     <img src="{{asset('frontend/AstromarG/balace-energy.jpg')}}" class="img-fluid " alt="...">
+              
+              <!----------------------------------Balance of Energies----------------------------------->
+            <div class="cad col-sm-12 col-md-6 bg-transparent">
+               
+                  <div class="for-img">
+                     <img src="{{asset('frontend/AstromarG/balace-energy.jpg')}}" class=" " alt="...">
                   </div>
-                  <div class="col-md-7">
-                     <div class="card-body">
+                  
+                     <div class="card-body pt-4">
                         <h5 class="card-title fw-bold fs-3">7] Balance of Energies: </h5>
                         <p class="card-text text-light">Vastu aims to balance the energies of a space by avoiding
                            clutter, obstruction, and imbalance in the arrangement of furniture and objects.</p>
                      </div>
                   </div>
-               </div>
-            </div>
-
+              
             <!--------------------------------- Remedies -------------------------------------------------->
-            <div class="card col-sm-12 col-md-6 mb-3 bg-transparent">
-               <div class="row g-0">
-                  <div class="col-md-5">
-                     <img src="{{asset('frontend/AstromarG/remedies.jpg')}}" class="img-fluid " alt="...">
+            <div class="cad col-sm-12 col-md-6 bg-transparent">
+               
+                  <div class="for-img">
+                     <img src="{{asset('frontend/AstromarG/remedies.jpg')}}" class=" " alt="...">
                   </div>
-                  <div class="col-md-7">
-                     <div class="card-body">
+                  
+                     <div class="card-body pt-4">
                         <h5 class="card-title fw-bold fs-3">8] Remedies: </h5>
                         <p class="card-text text-light">In cases where Vastu principles are not fully met due to
                            the
@@ -252,17 +215,15 @@
                         </p>
                      </div>
                   </div>
-               </div>
-            </div>
-
+              
             <!------------------------------- Residential and Commercial Spaces ------------------------------------>
-            <div class="card col-sm-12 col-md-6 mb-3 bg-transparent">
-               <div class="row g-0">
-                  <div class="col-md-5">
-                     <img src="{{asset('frontend/AstromarG/residential_commerical.jpg')}}" class="img-fluid " alt="...">
+            <div class="cad col-sm-12 col-md-6 bg-transparent">
+               
+                  <div class="for-img">
+                     <img src="{{asset('frontend/AstromarG/residential_commerical.jpg')}}" class=" " alt="...">
                   </div>
-                  <div class="col-md-7">
-                     <div class="card-body">
+                  
+                     <div class="card-body pt-4">
                         <h5 class="card-title fw-bold fs-3">9] Residential and Commercial Spaces: </h5>
                         <p class="card-text text-light">Vastu principles can be applied to homes, offices,
                            factories,
@@ -271,18 +232,15 @@
                         </p>
                      </div>
                   </div>
-               </div>
-            </div>
-
-
+              
             <!------------------------------------------- Cultural Variations ------------------------------------------>
-            <div class="card col-sm-12 col-md-6 mb-3 bg-transparent">
-               <div class="row g-0">
-                  <div class="col-md-5">
-                     <img src="{{asset('frontend/AstromarG/Variety of Traditions.jpg')}}" class="img-fluid " alt="...">
+            <div class="cad col-sm-12 col-md-6 bg-transparent">
+               
+                  <div class="for-img">
+                     <img src="{{asset('frontend/AstromarG/Variety of Traditions.jpg')}}" class=" " alt="...">
                   </div>
-                  <div class="col-md-7">
-                     <div class="card-body">
+                  
+                     <div class="card-body pt-4">
                         <h5 class="card-title fw-bold fs-3">10] Cultural Variations: </h5>
                         <p class="card-text text-light">Vastu Shastra is considered a science of architecture and
                            spatial arrangement, and its
@@ -290,9 +248,7 @@
                            prosperity, and spiritual well-being.</p>
                      </div>
                   </div>
-               </div>
-            </div>
-         </div>
+             </div>
       </div>
 </div>
 </section>
